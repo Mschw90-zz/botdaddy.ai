f9c3799663d63210fd7497eb60854f924e017f66
Matthew Schwartz <Mschw90@gmail.com>
2018-03-26 16:40:20 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI);
 
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.use('/', routes);
 
 app.listen(PORT, error => {
     error
