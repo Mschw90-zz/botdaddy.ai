67ad7937b98c033c0ce8db8a2e4e782e005eabde
Smitha <spallaki@brynmawr.edu>
2018-03-29 13:39:48 -0700

    Receieved messages JSON in correct format

--- connection/dialogFlow.js
+++ connection/dialogFlow.js
@@ -1,4 +1,4 @@
-//This code allows you to hook daddybot on Slack to our trained dialogflow bot. 
+//This code allows you to hook daddybot on Slack to our trained dialogflow bot.
 
 // You can find your project ID in your Dialogflow agent settings
 const projectId = 'botdaddy-ai'; //https://dialogflow.com/docs/agents#settings
@@ -6,14 +6,14 @@ const sessionId = 'quickstart-session-id'; //conversation id given by slack
 
 const query = 'hello';
 const languageCode = 'en-US';
- 
+
 // Instantiate a DialogFlow client.
 const dialogflow = require('dialogflow');
 const sessionClient = new dialogflow.SessionsClient();
- 
+
 // Define session path
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
- 
+
 // The text query request.
 const request = {
   session: sessionPath,
@@ -24,19 +24,20 @@ const request = {
     },
   },
 };
- 
+
 // Send request and log result
 sessionClient
   .detectIntent(request)
   .then(responses => {
-    console.log('Detected intent');
+    // console.log('Detected intent');
     const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
+    console.log(result.intent, "<<<<<<<<<<<RESULT");
+    // console.log(`  Query: ${result.queryText}`, "HELLO");
+    // console.log(`  Response: ${result.fulfillmentText}`);
     if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
+      // console.log(`  Intent: ${result.intent.displayName}`);
     } else {
-      console.log(`  No intent matched.`);
+      // console.log(`  No intent matched.`);
     }
   })
   .catch(err => {
