f91c33ce9ca8391f146c20d4049a0e1cd566ba08
staceykim4695 <staceykim4695@gmail.com>
2018-03-29 15:57:44 -0700

    connected slack to goog oauth

--- node.js
+++ node.js
@@ -11,11 +11,11 @@ rtm.on('message', (event) => {
   // For structure of `event`, see https://api.slack.com/events/reaction_added
   console.log(event);
   var message = event.text;
-
-
-rtm.addOutgoingEvent(true, 'message', { text:'hi you', channel: event.channel, reply_broadcast: true }).then((res) => {
-    // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
 });
+
+// rtm.addOutgoingEvent(true, 'message', { text:'hi you', channel: event.channel, reply_broadcast: true }).then((res) => {
+//     // `res` contains information about the posted message
+//     console.log('Message sent: ', res.ts);
+//   })
+//   .catch(console.error);
+// });
